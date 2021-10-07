734a7bddef97d4f876097ae117d471a40fa2b0ec
trollering12312 <trollering12312@gmail.com>
2021-10-07 17:50:48 +0900

    feat: Render ejs with indexRouter
    ejs와 index 사용해서 렌더링

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const path = require('path');
+
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 const indexRouter = require('./routes/index');
+const clubsRouter = require('./routes/clubs');
 
-app.use('/',indexRouter);
+app.use('/', indexRouter);
+app.use('/clubs', clubsRouter);
 
-server.listen(3000,() => {
-    console.log('Express server is now listening on localhost:3000...');
+server.listen(3000, () => {
+    console.log('Express Server is now listening on localhost:3000..');
 });
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.route('/').get((req,res,next) => {
-    return res.send('indexRouter works!')
+router.route('/').get((req, res, next) => {
+    res.render("index");
 });
 
 
