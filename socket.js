b64320338f47d8780bdb018e1de9c9834ad98fad
trollering12312 <trollering12312@gmail.com>
2021-10-07 19:00:41 +0900

    feat: broadcast message
    메세지를 화면에 출력

--- socket.js
+++ socket.js
@@ -1,11 +1,13 @@
 module.exports = (io) => {
     io.on('connection', (socket) => {
         console.log('A user is now connected');
-                
+
         socket.on('chat message', (msg) => {
+            io.emit('chat message', msg);
             console.log("message: ", msg);
         });
-        socket.on('disconnect', ()=>{
+
+        socket.on('disconnect', () => {
             console.log('disconnected');
         });
     });
